b10685221b71bd5a2836633e174dc794b971bad5
Roy Nyaga <nyagaandreroy@gmail.com>
2020-02-01 02:32:53 +0100

    added home about projects and services links on the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/services'
-  get 'pages/projects'
+  get  '/help',    to: 'static_pages#help'
+  get '/about', to: 'pages#about'
+  get '/services', to: 'pages#services'
+  get '/projects', to: 'pages#projects'
   root 'pages#home'
 
 end
