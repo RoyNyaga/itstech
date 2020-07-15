035b48237eabb70f46cff8e89bfe71c9ffbba580
Roy Nyaga <nyagaandreroy@gmail.com>
2020-07-15 02:41:03 +0100

    add home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,76 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+console.log("hello")
+// owl carousel
+$(document).ready(function () {
+  var owl = $('#logo')
+  owl.owlCarousel({
+    items: 4,
+    loop: true,
+    nav: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    smartSpeed: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      600: {
+        items: 4
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
+  $('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+  })
+  $('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+  })
+})
+
+// owl carousel
+$(document).ready(function () {
+  var owl = $('#customers')
+  owl.owlCarousel({
+    items: 4,
+    loop: true,
+    nav: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  })
+  $('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+  })
+  $('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+  })
+})
+
+
+
+
