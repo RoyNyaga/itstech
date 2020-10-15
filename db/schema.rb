b9bbcfb2740a0d167d61b4c349d8699c897c52d3
RoyNyaga <nyagaandreroy@gmail.com>
2020-10-14 22:24:01 -0700

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_051920) do
+ActiveRecord::Schema.define(version: 2020_10_15_052212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2020_10_15_051920) do
     t.string "slug"
     t.integer "review", default: 0
     t.integer "likes", default: 0
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
