bc207af198a017a510fc9d4db3f5a23a4c5a2871
RoyNyaga <nyagaandreroy@gmail.com>
2020-10-15 23:20:26 -0700

    create categories_controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
+    @categories = Category.all
     @blogs = Blog.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'categories/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :blogs
