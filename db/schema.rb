22f6982bfa1404f8b8c3439245c273dcb46b297f
RoyNyaga <nyagaandreroy@gmail.com>
2020-10-28 15:26:10 -0700

    add authors_link to blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_031810) do
+ActiveRecord::Schema.define(version: 2020_10_28_222349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_031810) do
   create_table "blogs", force: :cascade do |t|
     t.string "photo_link"
     t.string "title"
-    t.text "body" 
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_031810) do
     t.string "author_photo"
     t.string "time"
     t.string "category_name"
+    t.string "authors_link"
     t.index ["category_id"], name: "index_blogs_on_category_id"
     t.index ["category_name"], name: "index_blogs_on_category_name"
   end
